2486cc7038f869558fb8e6c8a3f57c4c28efa641
azfrondozo <afrondo@uw.edu>
2024-10-13 02:38:17 -0700

    Changes to API

--- FoodTrackAPI/index.js
+++ FoodTrackAPI/index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import serverless from "serverless-http";
 import cors from "cors";
-import { fetchUsers, createUser } from "./dynamo.js";
+import { fetchUsers, getUser, createUser, addFoodItem, deleteFoodItem } from "./dynamo.js";
+
 
 const app = express();
 const port = 3001;
 
-app.use(express.json);
+app.use(express.json());
+
 if (process.env.DEVELOPMENT) {
     app.use(cors());
 };
@@ -15,26 +17,49 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-app.get('/dynamo', async (req, res) => {
+app.get('/dynamo/:UserID', async (req, res) => {
     try {
-        const users = await fetchUsers();
-        res.send(users.Items)
+        const { UserID } = req.params;
+        const foods = await getUser(UserID);
+
+        res.send(foods)
     } catch (err) {
         res.status(400).send(`Error fetching users: ${err}`)
     }
   });
 
-// app.post('/', async (req, res) => {
-//   res.send('Hello World!')
-// });
+app.post('/dynamo', async (req, res) => {
+    try {
+        const parsedBody = typeof req.body === 'string' ? JSON.parse(req.body) : req.body;
+        const { UserID } = parsedBody;
 
-// app.put('/', async (req, res) => {
-//     res.send('Hello World!')
-// });
+        const response = await createUser(UserID);
+        res.send(response)
+    } catch (err) {
+        res.status(400).send(`Error creating user: ${err}`);
+    }
+});
 
-// app.delete('/', async (req, res) => {
-//   res.send('Hello World!')
-// });
+app.put('/dynamo', async (req, res) => {
+    try {
+        const { username, foodItem, expirationDate } = req.body;
+        const response = await addFoodItem(username, foodItem, expirationDate);
+        res.send(response)
+    } catch (err) {
+        res.status(400).send(`Error updating food items: ${err}`);
+    }
+});
+
+app.delete('/dynamo/:UserID/foods', async (req, res) => {
+    try {
+        const { UserID } = req.params;  // Extract UserId from the URL parameters
+        const { foodItem } = req.body;  // Get the foodItem to delete from the request body
+        const response = await deleteFoodItem(UserID, foodItem);  // Call the deleteFoodItem function
+        res.status(200).send(response);
+    } catch (err) {
+        res.status(400).send(`Error deleting food item: ${err}`);
+    }
+});
 
 if (process.env.DEVELOPMENT) {
     app.listen(port, () => {
